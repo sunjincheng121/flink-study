a143510c3b8825090113acef6ebb695be95f649b
仲炜 <zw144119@alibaba-inc.com>
2018-12-06 11:09:32 +0800

    add hint

--- src/test/scala/org/apache/flink/study/table/TestClass.scala
+++ src/test/scala/org/apache/flink/study/table/TestClass.scala
@@ -3,6 +3,12 @@ package org.apache.flink.study.table
 import org.apache.flink.api.common.typeinfo.TypeInformation
 
 class TestClass {
+
+  /**
+    * Scala method call: two method with same name and same signature, one of them has a implicit value
+    * and another doesn't have. In this situation, that one without implicit value must not be a generic
+    * method. If both of them are generic methods, an compile error will occur.
+    */
   def test[T: TypeInformation](cls: Class[T]): Unit = {
     println("with typeinfo: " + cls.toString + ", typeinfo: " + implicitly[TypeInformation[T]].toString)
   }
